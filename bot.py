6de8236b9e6b642cdb0c66655196ee3e0cc7502a
Sharma123321 <sharmadhruv43us@gmail.com>
2026-07-14 23:58:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -382,7 +382,7 @@ def handle_menu_clicks(call):
         _,brand_id,model_id=call.data.split("_",2)
         try:
             model_info = device_data[brand_id]["models"][model_id]
-    qr_path = os.path.join(BASE_DIR, model_info["qr"])
+            qr_path = os.path.join(BASE_DIR, model_info["qr"])
 
     if not os.path.exists(qr_path):
         qr_path = QR_IMAGE_PATH
